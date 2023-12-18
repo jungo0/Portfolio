ae15dcf1920c79a80f835768d3564cd0e8b64051
jungo <81670100+jungo0@users.noreply.github.com>
2023-12-18 22:13:32 +0900

    feat:refactoring

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,11 +1,11 @@
 import React from "react";
 import styled from "styled-components";
-import vid1 from "./video/1.mov";
-import vid2 from "./video/2.mov";
+import vid1 from "./video/1.mp4";
+import vid2 from "./video/2.mp4";
 
 const images = [];
-for (let i = 1; i <= 35; i++) {
-  images.push(require(`../public/img/${i}.png`));
+for (let i = 4; i <= 35; i++) {
+  images.push(require(`../public/img/${i}.jpg`));
 }
 
 const ImageContainer = styled.div`
@@ -16,8 +16,8 @@ const ImageContainer = styled.div`
 
 const MediaContainer = styled.div`
   position: relative;
-  width: 100%;
-  margin: 15px;
+  width: calc(100% - 100px);
+  margin: 0px 250px;
 `;
 
 const Image = styled.img`
@@ -26,18 +26,12 @@ const Image = styled.img`
 
 const Video = styled.video`
   position: absolute;
-  top: 50%;
-  left: 50%;
+  top: 46%;
+  left: 49.5%;
   transform: translate(-50%, -50%);
-  width: 100%;
-  height: auto;
-  max-width: 350px;
-  max-height: 350px;
+  width: 55%;
+  height: 47%;
   cursor: pointer;
-
-  @media (max-width: 600px) {
-    max-width: 50%;
-  }
 `;
 function App() {
   const handleVideoClick = (index) => {
@@ -58,7 +52,7 @@ function App() {
           {images.map((src, index) => (
             <MediaContainer key={index} onClick={() => handleVideoClick(index)}>
               <Image src={src} alt={`img${index + 1}`} />
-              {(index === 0 || index === 1) && (
+              {(index === 29 || index === 30) && (
                 <Video loop muted id={`video${index}`}>
                   <source src={index === 0 ? vid1 : vid2} type="video/mp4" />
                 </Video>
