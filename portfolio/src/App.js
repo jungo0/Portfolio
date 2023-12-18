3c2a759eb88dd18bab85eccda705f5cdef140d21
jungo <81670100+jungo0@users.noreply.github.com>
2023-12-18 22:42:02 +0900

    feat:썸네일추가, 간격없애기

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,7 +1,9 @@
-import React from "react";
+import React, { useState } from "react";
 import styled from "styled-components";
 import vid1 from "./video/1.mp4";
 import vid2 from "./video/2.mp4";
+import th1 from "./th1.jpg";
+import th2 from "./th2.jpg";
 
 const images = [];
 for (let i = 4; i <= 35; i++) {
@@ -17,7 +19,13 @@ const ImageContainer = styled.div`
 const MediaContainer = styled.div`
   position: relative;
   width: calc(100% - 100px);
-  margin: 0px 250px;
+  margin: ${(props) => (props.noMargin ? "0 250px" : "0px 250px")};
+  overflow: hidden;
+  ${(props) =>
+    (props.noMargin && props.index >= 17 && props.index <= 21) ||
+    (!props.noMargin && props.index >= 22 && props.index <= 24)
+      ? "line-height: 0;"
+      : ""};
 `;
 
 const Image = styled.img`
@@ -33,10 +41,29 @@ const Video = styled.video`
   height: 47%;
   cursor: pointer;
 `;
+
+const Thumbnail = styled.img`
+  position: absolute;
+  top: 46%;
+  left: 49.5%;
+  transform: translate(-50%, -50%);
+  width: 25%;
+  height: 47%;
+  cursor: pointer;
+  opacity: ${(props) => (props.isHidden ? 0 : 1)};
+`;
+
 function App() {
+  const [thumbnailStates, setThumbnailStates] = useState([false, false]);
+
   const handleVideoClick = (index) => {
     const video = document.getElementById(`video${index}`);
     if (video) {
+      const updatedStates = [...thumbnailStates];
+      updatedStates[index === 29 ? 0 : 1] =
+        !thumbnailStates[index === 29 ? 0 : 1];
+      setThumbnailStates(updatedStates);
+
       if (video.paused) {
         video.play();
       } else {
@@ -50,12 +77,26 @@ function App() {
       <header className="App-header">
         <ImageContainer>
           {images.map((src, index) => (
-            <MediaContainer key={index} onClick={() => handleVideoClick(index)}>
+            <MediaContainer
+              key={index}
+              onClick={() => handleVideoClick(index)}
+              noMargin={
+                (index >= 17 && index <= 21) || (index >= 22 && index <= 24)
+              }
+              index={index}
+            >
               <Image src={src} alt={`img${index + 1}`} />
               {(index === 29 || index === 30) && (
-                <Video loop muted id={`video${index}`}>
-                  <source src={index === 0 ? vid1 : vid2} type="video/mp4" />
-                </Video>
+                <>
+                  <Video loop muted={false} id={`video${index}`}>
+                    <source src={index === 29 ? vid1 : vid2} type="video/mp4" />
+                  </Video>
+                  <Thumbnail
+                    src={index === 29 ? th1 : th2}
+                    alt={`thumbnail${index + 1}`}
+                    isHidden={thumbnailStates[index === 29 ? 0 : 1]}
+                  />
+                </>
               )}
             </MediaContainer>
           ))}
